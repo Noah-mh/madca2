0141778dc2adbf6a3f09f0fab391d301675c581a
Noah <lwin.moehtet77@gmail.com>
2023-01-24 21:31:00 +0800

    update

--- component/register2.js
+++ component/register2.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from "react";
 import {
   SafeAreaView,
   View,
@@ -6,36 +6,65 @@ import {
   TextInput,
   StyleSheet,
   StatusBar,
-} from 'react-native';
+} from "react-native";
 
-import AppLogo from './AppLogo';
-import CustomButton from './CustomButton';
-import ProgressLine from './progressLine';
+import AppLogo from "./AppLogo";
+import CustomButton from "./CustomButton";
+import ProgressLine from "./progressLine";
+/* import FirebaseApp from "../firebase";
+
+const auth = FirebaseApp.auth(); */
 
 export default RegisterScreen2 = ({ navigation }) => {
-  const [email, setEmail] = React.useState('');
-  const [password, setPassword] = React.useState('');
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+/* 
+  const onHandleSignUp = async () => {
+    try {
+      if (email != "" && password != "") {
+        await auth.createUserWithEmailAndPassword(email, password);
+        setSignupStatus("created user ok");
+      }
+    } catch (error) {
+      alert(error.message);
+    }
+  }; */
+
   return (
     <SafeAreaView style={styles.container}>
       <AppLogo />
-      <View style={{ alignItems: 'center', marginTop: 150 }}>
+      <View style={{ alignItems: "center", marginTop: 80 }}>
+        <View style={styles.inputitems}>
+          <Text style={styles.subhead}>Username</Text>
+          <TextInput
+            style={styles.input}
+            underlineColorAndroid="transparent"
+            placeholder={""}
+            placeholderTextColor="black"
+            autoCapitalize="none"
+            value={username}
+            onChangeText={(text) => setUsername(text)}
+          ></TextInput>
+        </View>
         <View style={styles.inputitems}>
           <Text style={styles.subhead}>Email Address</Text>
           <TextInput
             style={styles.input}
             underlineColorAndroid="transparent"
-            placeholder={''}
+            placeholder={""}
             placeholderTextColor="black"
             autoCapitalize="none"
             value={email}
-            onChangeText={(text) => setEmail(text)}></TextInput>
+            onChangeText={(text) => setEmail(text)}
+          ></TextInput>
         </View>
         <View style={styles.inputitems}>
           <Text style={styles.subhead}>Password</Text>
           <TextInput
             style={styles.input}
             underlineColorAndroid="transparent"
-            placeholder={''}
+            placeholder={""}
             secureTextEntry={true}
             placeholderTextColor="black"
             autoCapitalize="none"
@@ -48,7 +77,7 @@ export default RegisterScreen2 = ({ navigation }) => {
           <TextInput
             style={styles.input}
             underlineColorAndroid="transparent"
-            placeholder={''}
+            placeholder={""}
             secureTextEntry={true}
             placeholderTextColor="black"
             autoCapitalize="none"
@@ -66,14 +95,15 @@ export default RegisterScreen2 = ({ navigation }) => {
           Use 8 or more characters with a mix of letters, numbers & symbols.
         </Text>
       </View>
-      <View style={{ alignItems: 'center' }}>
+      <View style={{ alignItems: "center" }}>
         <CustomButton
           text="Register"
           color="white"
           backgroundColor="#B21818"
           marginBottom={25}
-          onPress={() => navigation.navigate('BottomBar')}></CustomButton>
-        <Text style={{ color: 'white', marginTop: 30, marginBottom: 40 }}>
+          onPress={ () => navigation.navigate("BottomBar")}
+        ></CustomButton>
+        <Text style={{ color: "white", marginTop: 30, marginBottom: 40 }}>
           Do you have already an account?
         </Text>
         <CustomButton
@@ -81,7 +111,8 @@ export default RegisterScreen2 = ({ navigation }) => {
           color="white"
           backgroundColor="#666680"
           marginBottom={25}
-          onPress={() => navigation.navigate('SignIn')}></CustomButton>
+          onPress={() => navigation.navigate("SignIn")}
+        ></CustomButton>
       </View>
     </SafeAreaView>
   );
@@ -90,9 +121,9 @@ export default RegisterScreen2 = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'space-between',
-    width: '100%',
-    backgroundColor: '#1C1C23',
+    justifyContent: "space-between",
+    width: "100%",
+    backgroundColor: "#1C1C23",
     marginTop: StatusBar.currentHeight || 0,
   },
 
@@ -100,30 +131,31 @@ const styles = StyleSheet.create({
     marginBottom: 20,
   },
   subhead: {
-    color: '#6E548C',
+    color: "#6E548C",
+    fontSize: 16,
   },
 
   input: {
     borderRadius: 10,
-    borderColor: '#6E548C',
+    borderColor: "#6E548C",
     width: 350,
     height: 45,
     borderWidth: 1,
   },
   line: {
     borderRadius: 20,
-    alignItems: 'center',
-    justifyContent: 'center',
-    flexDirection: 'row',
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
     width: 85,
     marginTop: 10,
     marginBottom: 10,
   },
 
   termAndCondition: {
-    color: '#6E548C',
+    color: "#6E548C",
     margin: 25,
-    textAlign: 'left',
+    textAlign: "left",
     width: 330,
   },
 });
